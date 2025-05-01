5e0d697d7821693b4a885806fe946fb1dbbafc76
Minjae Jeong <wjdalswo0312@naver.com>
2025-05-01 12:34:01 +0900

    Update index.tsx

--- ui/app/index.tsx
+++ ui/app/index.tsx
@@ -1,31 +1,42 @@
 import CompleteButton from "@/components/CompleteButton";
 import TextField from "@/components/InputField";
 import ToggleButton from "@/components/ToggleButton";
-import { Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 import { useState } from "react";
 import AddressBox from "@/components/AddressBox";
 import BottomTabBar from "@/components/BottomTabBar";
 import ChatFilterTab from "@/components/ChatFilterTab";
-import MatchingPage from "./MatchingPage";
 import LikabilityBar from "@/components/LikabilityBar";
+import MbtiSlider from "@/components/MbtiSlider";
+import LikertSlider from "@/components/LikertSlider";
 
 
 export default function Index() {
   const [gender, setGender] = useState('');
+  const [ie, setIe] = useState(50);
+  const [value1, setValue1] = useState(3);
+
   return (
     <View>
-      <ChatFilterTab tabs={['All', 'BetaBae', 'RealBae']} />
-      <TextField label="Email" placeholder="Please enter." />
-      <AddressBox />
-      <ToggleButton
-        label="Gender"
-        options={['Male', 'Female']}
-        selected={gender}
-        onSelect={(option) => setGender(option)}
-      />
-      <LikabilityBar percent={80} />
-      <CompleteButton title="Complete"/>
-
+      <ScrollView>
+        <ChatFilterTab tabs={['All', 'BetaBae', 'RealBae']} />
+        <TextField label="Email" placeholder="Please enter." />
+        <AddressBox />
+        <ToggleButton
+          label="Gender"
+          options={['Male', 'Female']}
+          selected={gender}
+          onSelect={(option) => setGender(option)}
+        />
+        <LikabilityBar percent={80} />
+        <MbtiSlider labelLeft="Introverted" labelRight="Extrovert" value={ie} onChange={setIe} />
+        <LikertSlider
+          question="1. Gets nervous easily"
+          value={value1}
+          onChange={setValue1}
+        />
+        <CompleteButton title="Complete"/>
+      </ScrollView>
       <BottomTabBar />
     </View>
   );
