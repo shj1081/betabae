9fba7bf286b02ae64a38e54a36c6127fad87bd86
Minjae Jeong <wjdalswo0312@naver.com>
2025-05-01 13:33:29 +0900

    Create colors.ts

---
+++ ui/constants/colors.ts
@@ -0,0 +1,10 @@
+const COLORS = {
+    MAIN: '#C52B67',         // 메인 색상
+    SUB: '#C13448',          // 서브 강조 색상
+    DARK_GRAY: '#696969',    // 텍스트 등 짙은 회색
+    LIGHT_GRAY: '#EBEBEB',   // 연한 회색 (배경, 보더 등)
+    BLACK: '#000000',        // 검정
+    WHITE: '#FFFFFF',        // 흰색
+  };
+  
+  export default COLORS;
